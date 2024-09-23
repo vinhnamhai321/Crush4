4feb695e095b5b664c346c723910801606095200
Rito <116003382+vinhnamhai321@users.noreply.github.com>
2024-09-23 19:27:40 +0700

    change

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Anh hứa hongg làm bé buồn nữa đouuu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
-    "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "Anh bicc lỗi rồi mừ",
+    "Mong em tha lỗi choo anhh :((",
+    "Anhh saii rồi , anh đáng trách ạ",
+    "nhemm nhưng nhận nhanh nữa nha",
+    "anh còn mỗi 3 mạng thôi huhu |",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
